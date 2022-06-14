569ba8438af49c5da2d9721a6ea2ce47059ffaea
YAZAN <YAZAN@DESKTOP-SVUSALT>
2022-06-15 00:44:33 +0300

    Add project files.

---
+++ LaTow/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace LaTow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtusernae = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtconpass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btnUpdata = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtusernae
+            // 
+            this.txtusernae.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtusernae.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtusernae.Location = new System.Drawing.Point(223, 47);
+            this.txtusernae.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtusernae.Name = "txtusernae";
+            this.txtusernae.Size = new System.Drawing.Size(163, 22);
+            this.txtusernae.TabIndex = 0;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtpassword.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtpassword.Location = new System.Drawing.Point(223, 114);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(163, 22);
+            this.txtpassword.TabIndex = 0;
+            // 
+            // txtconpass
+            // 
+            this.txtconpass.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtconpass.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtconpass.Location = new System.Drawing.Point(223, 181);
+            this.txtconpass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtconpass.Name = "txtconpass";
+            this.txtconpass.Size = new System.Drawing.Size(163, 22);
+            this.txtconpass.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "user name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 120);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 187);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = " password";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dataView
+            // 
+            this.dataView.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Location = new System.Drawing.Point(482, 47);
+            this.dataView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataView.Name = "dataView";
+            this.dataView.RowHeadersWidth = 51;
+            this.dataView.Size = new System.Drawing.Size(226, 156);
+            this.dataView.TabIndex = 2;
+            this.dataView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataView_CellContentClick);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(89, 270);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(100, 28);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(313, 270);
+            this.btn_delete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(100, 28);
+            this.btn_delete.TabIndex = 3;
+            this.btn_delete.Text = "delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btnUpdata
+            // 
+            this.btnUpdata.Location = new System.Drawing.Point(524, 270);
+            this.btnUpdata.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUpdata.Name = "btnUpdata";
+            this.btnUpdata.Size = new System.Drawing.Size(100, 28);
+            this.btnUpdata.TabIndex = 3;
+            this.btnUpdata.Text = "Updata";
+            this.btnUpdata.UseVisualStyleBackColor = true;
+            this.btnUpdata.Click += new System.EventHandler(this.btnUpdata_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 361);
+            this.Controls.Add(this.btnUpdata);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.dataView);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtconpass);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusernae);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "User";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtusernae;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtconpass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btnUpdata;
+    }
+}
+
